4f25759f1b335e7e35d5e3b112c401282e240b9b
Adam Siekerski <adam.siekerski@aluplast.com.pl>
2020-01-18 13:01:34 +0100

    test

--- UserManager.Infrastructure/Service/FileService.cs
+++ UserManager.Infrastructure/Service/FileService.cs
@@ -1,6 +1,14 @@
-﻿namespace UserManager.Infrastructure.Service
+﻿using System.Collections.Generic;
+using UserManager.Model;
+
+namespace UserManager.Infrastructure.Service
 {
     public class FileService
     {
+        public List<Person> ReadFromFile(string path)
+        {
+            var personsList =new List<Person>();
+            return personsList;
+        }
     }
 }
